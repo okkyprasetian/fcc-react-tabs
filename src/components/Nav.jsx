ee4c16805566056e66b585b848299286f078a4da
okkyprasetian <okkyprasetian@gmail.com>
2021-11-10 08:45:26 +0700

    basic styling for convenient coding experince

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,7 @@
 function Nav() {
     return (
         <nav className="nav">
-            <h2>Tommy</h2>
+            <h2>TOMMY</h2>
             <h2>BIGDROP</h2>
             <h2>CUKER</h2>
         </nav>
